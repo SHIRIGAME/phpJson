1814204e3eac5eb9b61753978fe46ac4a72497f6
xysden_ <88167094+SHIRIGAME@users.noreply.github.com>
2026-03-12 09:25:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<?php
+ class Produto{
+   public $codProd; 
+   public $nome;
+   public $unid;
+   public $saldoEstoque;
+ }
+public function __construct($codProd,$nome,$unid,$saldoEstoque){
+  $this -> codProd = $codprod;
+  $this -> nome = $nome;
+  $this -> unid = $unid; 
+  $this -> saldoEstoque = $saldoestoque
 
+public function entrada ($quantidade){
+   $this -> saldoEstoque += $quantidade;
+}
+public function saida ($quantidade){
+  if ($quantidade <= $this -> saldoEstoque){
+     $this -> saldoEstoque -= $quantidade;
+  }
+  else{
+    echo "Saldo Insuficiente! ";
+  }
+      
