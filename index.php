c6e2e8bff4647356d41ec054f689a65f83eed67d
xysden_ <88167094+SHIRIGAME@users.noreply.github.com>
2026-03-15 10:57:28 -0300

    Add product management and JSON export functionality

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-
+<?php
+ require "produto.php";
+  $produto = new Produto (10, "Feijão", "KG",10);
+  $produto -> entrada (5):
+  $produto -> saida (2);
+ $json = json_encode($produto ->toArray(),JSON_PRETTY_PRINT);
+ file_put_contents("estoque.json",$json);
+ echo $json;
